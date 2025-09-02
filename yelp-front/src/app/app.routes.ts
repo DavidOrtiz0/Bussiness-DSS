b4ec1c0340090d3841d008450e14d8e6b7ce8f01
Maria Paula Parga <maria.parga39339@ucaldas.edu.co>
2025-09-02 14:48:43 -0500

    Intento 2. Cargar widget de soporte

--- yelp-front/src/app/app.routes.ts
+++ yelp-front/src/app/app.routes.ts
@@ -3,11 +3,11 @@
 import { Routes } from '@angular/router';
 import { FileUpload } from './components/file-upload/file-upload';
 import { Dashboard } from './components/dashboard/dashboard';
+import { SupportWidget } from './components/support-widget/support-widget';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'upload', pathMatch: 'full' },
   { path: 'upload', component: FileUpload },
   { path: 'dashboard', component: Dashboard },
   { path: '**', redirectTo: 'upload' }
-
 ];
