df9a5b53a53f6e90a79bd019cf3fbcc70fe11c02
MariaPaulaTovar01 <161061115+MariaPaulaTovar01@users.noreply.github.com>
2025-09-02 09:40:09 -0500

    Delete frontend/frontend-DSS directory
    maldita cosa

--- frontend/frontend-DSS/src/app/services/support.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, signal } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class SupportService {
-  panelOpen = signal(false);
-  openPanel(){ this.panelOpen.set(true); }
-  closePanel(){ this.panelOpen.set(false); }
-  togglePanel(){ this.panelOpen.update(v => !v); }
-}
